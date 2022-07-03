d1aff6387cd56dd4f05891acb600e65f0d0a55dd
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-03 20:19:31 +0900

    feat: First commit

---
+++ asciidoc.mk
@@ -0,0 +1,14 @@
+ROUGE_STYLE=monokai_sublime
+
+AD_OPTS_HTML5_COMMON=\
+--backend=html5 \
+-a imagesdir=images
+
+AD_OPTS_HTML5_ROUGE=\
+$(AD_OPTS_HTML5_COMMON) \
+-r asciidoctor-diagram \
+-a source-highlighter=rouge \
+-a rouge-style=$(ROUGE_STYLE)
+
+AD_OPTS_HTML5_STEM=\
+$(AD_OPTS_HTML5_COMMON)
