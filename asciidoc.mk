d7c4eae7abf66533d36ada1266bab1bc5f6286c8
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-03 21:52:44 +0900

    docs: Fix broken links.

--- asciidoc.mk
+++ asciidoc.mk
@@ -1,8 +1,10 @@
+IMAGES_DIR=images
+CP_OPTS=-n -l
 ROUGE_STYLE=monokai_sublime
 
 AD_OPTS_HTML5_COMMON=\
 --backend=html5 \
--a imagesdir=images
+-a imagesdir=$(IMAGES_DIR)
 
 AD_OPTS_HTML5_ROUGE=\
 $(AD_OPTS_HTML5_COMMON) \
